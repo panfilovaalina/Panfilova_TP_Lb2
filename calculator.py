4e4c9202f035a1e9892573fda91140b0b7c0f28c
DenisBlokh <denys.blokh.cpe@nure.ua>
2025-07-10 10:05:41 +0300

    Add basic calculator functions (add, subtract)

--- calculator.py
+++ calculator.py
@@ -11,4 +11,10 @@ if __name__ == "__main__":
 num2 = 7
 print(f"{num1} + {num2} = {add(num1, num2)}")
 print(f"{num1} - {num2} = {subtract(num1, num2)}")
-print("Базовий калькулятор готовий.") # Цей рядок будемо змінювати
+
+if __name__ == "__main__":
+    num1 = 15
+    num2 = 7
+    print(f"{num1} + {num2} = {add(num1, num2)}")
+    print(f"{num1} - {num2} = {subtract(num1, num2)}")
+    # Тут немає виклику multiply! print("Виправлено текст повідомлення у main.") # Змінений той самий рядок!
